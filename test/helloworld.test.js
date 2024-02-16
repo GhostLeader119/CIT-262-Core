bdc6402efc6084222252651c26283684bece18b6
GhostLeader119 <cadecamp9@gmail.com>
2024-02-16 16:44:48 -0700

    stedi code
    added stedi code

---
+++ test/helloworld.test.js
@@ -0,0 +1,9 @@
+import sayHello from '../utils/helloworld.mjs';
+import assert from 'assert';
+it("Test Hello World", ()=>{
+    const hello = sayHello();
+
+    assert.equal(hello,"hello");
+
+    console.log(hello);
+});
