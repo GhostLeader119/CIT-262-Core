edc1b03fa62b699d26d184461e75db271a272c50
GhostLeader119 <cadecamp9@gmail.com>
2024-02-16 16:56:20 -0700

    added my name to test

--- test/helloworld.test.js
+++ test/helloworld.test.js
@@ -1,9 +1,11 @@
 import sayHello from '../utils/helloworld.mjs';
 import assert from 'assert';
 it("Test Hello World", ()=>{
-    const hello = sayHello();
+    const hello = sayHello(1);
+    // const failure_test = sayHello(2); Wanted to see what multiple tests did, mixing successes and failures.
 
-    assert.equal(hello,"hello");
+    assert.equal(hello,"hello caden");
+    // assert.equal(failure_test,"hello caden");
 
     console.log(hello);
 });
--- utils/helloworld.mjs
+++ utils/helloworld.mjs
@@ -1,5 +1,11 @@
-const sayHello=()=>{
-    return "hello";
+const sayHello=(input1)=>{
+    if (input1 = 1) {
+    const output1 = "hello caden";
+    return output1}
+    else {
+    const output1 = "birds";
+    return output1;}
+    
 }
 
 export default sayHello;
